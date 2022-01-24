09e52150da3e5e969d5119988b46fd2d4d659bdf
Dhanasri <dhanasreepothineni@gmail.com>
2022-01-24 12:07:47 +0530

    Changed the status code to 201 created

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
 
 import java.util.Optional;
 
@@ -30,6 +33,7 @@ public class UserController {
 	}
 
 	@PostMapping("/user")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	void createUser(@RequestBody User user) {
 		System.out.println(user.getName());
 		userService.saveUser(user);
