179e42eb5809f1d276997785e4a02fd3993460e1
Dhanasri <dhanasreepothineni@gmail.com>
2022-01-24 15:42:26 +0530

    Deleted by ID

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -40,8 +40,8 @@ public class UserController {
 		userService.saveUser(user);
 	}
 
-	@DeleteMapping("/user")
-	void deleteUser() {
-		// Removed the word comments
+	@DeleteMapping("/user/{id}")
+	 void deleteUser(@PathVariable("id") Integer id) {
+		 userService.deleteUser(id);
 	}
 }
